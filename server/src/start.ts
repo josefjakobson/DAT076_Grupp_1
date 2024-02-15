c82b6826c37bc27534e7aa4cc0d62281feaf80e6
Zaloz64 <Zoe@zalo.se>
2024-02-15 10:23:46 +0100

    setup done

--- server/src/start.ts
+++ server/src/start.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from "cors";
+
 import { userRouter } from "./router/userRouter";
 
 export const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/userRouter", userRouter);
 
 

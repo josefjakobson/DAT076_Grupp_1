75f31641958219e2212e34f88f2ab087083eff2e
josefjakobson <j.jakobson28@gmail.com>
2024-03-08 16:51:31 +0100

    cont. router tests

--- server/src/router/userRouterTest.test.ts
+++ server/src/router/userRouterTest.test.ts
@@ -5,8 +5,8 @@ import { User } from "../model/user";
 const request = SuperTest.default(app);
 
 test("Get request to /user", async () => {
-    const res1 = await request.get("/user").send({username : "user1", password: "123"});
-    console.log(res1);
+    const res1 = await request.get("/user").send({});
+    console.log(res1.headers, res1.statusCode);
     expect(res1.statusCode).toEqual(200);
     expect(res1.body).toEqual({username : "user1", password: "123"});
     const res2 = await request.get("/user");
--- server/src/start.ts
+++ server/src/start.ts
@@ -6,6 +6,6 @@ export const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/userRouter", userRouter);
+app.use("/user", userRouter);
 
 
